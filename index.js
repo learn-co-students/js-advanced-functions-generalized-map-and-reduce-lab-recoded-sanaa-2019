cb46c3980876b10b2e247ab6d7fe2055b424065a
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-10-03 21:02:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+const map = function(arr  , fun){
+    return arr.map(fun);
+}
+const reduce = function(arr  , fun , start = true  ){
+
+    if(typeof arr.reduce(fun ,start ) === 'number' && start == 1){
+        return arr.reduce(fun ,start ) -1;
+    }
+    else{
+        return arr.reduce(fun ,start ) ;
+    }
+}
