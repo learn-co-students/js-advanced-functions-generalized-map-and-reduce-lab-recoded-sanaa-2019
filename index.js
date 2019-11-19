ba16858abedce77f77215dbdd8821fc8732972bc
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-11-19 07:00:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // Add your functions here
-/*const map = function(arr  , fun){
-    return arr.map(fun);
-}
-const reduce = function(arr  , fun , start = true  ){
-
-    if(typeof arr.reduce(fun ,start ) === 'number' && start == 1){
-        return arr.reduce(fun ,start ) -1;
-    }
-    else{
-        return arr.reduce(fun ,start ) ;
-    }
-}*/
 
 const map = function(srArr, srFunction){
     let myArr= srArr.map(srFunction);
