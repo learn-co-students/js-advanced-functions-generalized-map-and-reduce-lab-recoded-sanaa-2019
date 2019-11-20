6565078fa4450a56ba59a3d43e018ab8a067e50b
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-20 20:05:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions here
+function map(array, newFunc){
+    let arr = [];
+      console.log(arr)
+      array.forEach(element => {
+          arr.push(newFunc(element))
+      });
+      return arr
+    }
+    console.log(arr)
+
+
+
+
+    function reduce(array , myFunc, newVal ){
+
+    let Start;  
+    if(!!array[0]){ Start=true; Start=Boolean(Start);} 
+    if(newVal){ Start=newVal;}
+
+    function number(array) {
+        return typeof(array) === "number";
+      }
+      
+      
+    if(!newVal && array.every(number)){
+    Start=0
+      
+    }
+
+   for(let i=0; i<array.length; i++){  
+            Start = myFunc(array[i],Start )
+        }
+
+  return Start ; 
+}
+  
+  
