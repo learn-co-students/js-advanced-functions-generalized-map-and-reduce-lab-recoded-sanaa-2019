9f9e16cc2eca4a566fc6d21922936ed6872552af
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-04 08:38:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+let map = function(sourceArray,sourceFunction){
+    return sourceArray.map(sourceFunction);
+  }
+let reduce = function(sourceArray,callBack,startingPoint){
+    if(startingPoint !== undefined){
+      return sourceArray.reduce(callBack,startingPoint);
+    }
+    return sourceArray.reduce(callBack);
+  }
