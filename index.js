6aa18377d1632ca40eed7749c0fad8fb4079646c
Firas Al-Azizy <firasazizy614@gmail.com>
2019-12-06 14:38:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+const map = function(array,func){
+        return array.map(func);
+    }
+const reduce = function(array,func,val=true){
+    
+let x=1;
+      
+  if (typeof array.reduce(func,val) === 'number' && val == x)
+  
+      {return array.reduce(func,val) -1}
+  else 
+  
+      {return array.reduce(func,val)}
+    }
