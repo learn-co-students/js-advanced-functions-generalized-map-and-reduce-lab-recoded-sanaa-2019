d4367fa03b279513e1ce87f3f5e050cb82ab4b57
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 21:43:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ function mapToNegativize(src) {
   return r
 }
 
-function map(src) {
-  let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(src[i]) // Unique work
-  }
-  return r
-}
+map([1, 2, 3, -9], function(a){ return -1 * a }
 
 function mapToDouble(src) {
   let r = []
